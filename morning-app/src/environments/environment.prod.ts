068b0051e4a0450df661812006b012f2422fec00
Amar Jasarbasic <amarjasarbasic@gmail.com>
2020-02-16 11:39:07 -0800

    added prod env

---
+++ morning-app/src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCb1t8S5Z7kVibR1kFDNlpQxiOGyeZLofo",
+    authDomain: "wakeup-9fbd3.firebaseapp.com",
+    databaseURL: "https://wakeup-9fbd3.firebaseio.com",
+    projectId: "wakeup-9fbd3",
+    storageBucket: "wakeup-9fbd3.appspot.com",
+    messagingSenderId: "1026287447856",
+    appId: "1:1026287447856:web:872340677e8830d0a322f5",
+    measurementId: "G-S8E4ZRPHYX"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
